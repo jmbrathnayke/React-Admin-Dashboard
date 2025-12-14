28142d2eabab337efca4ca904db97e25b243044b
Manujayaa <janithrathnayake01@gmail.com>
2025-12-14 20:21:18 +0530

    index.css import updated

--- Admin_Dashboard/src/main.jsx
+++ Admin_Dashboard/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "./Context/ThemeContext";
 
-//
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
